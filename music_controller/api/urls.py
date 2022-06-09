aeeea90bcc0a0523ca44127556ebd70b8f0c20f5
Thamognya Kodi <contact@thamognya.com>
2022-06-09 23:27:48 +0700

    some comments

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
 
+# /api/room contains the view of the Room model
+
 urlpatterns = [
     path('room', RoomView.as_view(), name='index'),
 ]
--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -4,6 +4,9 @@ from .models import Room
 from .serializers import RoomSerializer
 # Create your views here.
 
+# get the view setup
 class RoomView(generics.ListAPIView):
+    # to get all the of the Room objects
     queryset = Room.objects.all()
+    # the serializer class to use from ./seriaizers.py
     serializer_class = RoomSerializer
